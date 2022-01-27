f538ab42115d0431e7b6fc6b43ce8a740e8a6143
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-28 01:51:12 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name:"yardım",
+  code:`
+  $author[Selammm;$authorAvatar]
+  $description[
+**$getServerVar[prefix]play :** Şarkıyı başlatır
+**$getServerVar[prefix]skip :** Şarkıyı geçer
+**$getServerVar[prefix]pause :** Şarkıyı durdurur
+**$getServerVar[prefix]resume :** Şarkıyı devam ettirir
+**$getServerVar[prefix]loop :** Şarkıyı tekrarlar
+**$getServerVar[prefix]queue :** Şarkı sırasını atar
+**$getServerVar[prefix]volume :** Şarkı sesini ayarlar
+**$getServerVar[prefix]leave :** Şarkıyı sonlandırır]
+$color[BLACK]
+$thumbnail[$authorAvatar]
+
+`
+  }
